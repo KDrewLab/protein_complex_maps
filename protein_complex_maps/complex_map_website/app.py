97ab005e3bcef10a6871d4c950735b775c9c622e
Kevin Drew <ksdrew@uic.edu>
2025-05-20 13:26:01 -0500

    Removed complex_map_website.py, now app.py. Updated app.py to direct root '/' to humap3 front page rather than proteincomplexes.org front page

--- protein_complex_maps/complex_map_website/app.py
+++ protein_complex_maps/complex_map_website/app.py
@@ -110,9 +110,14 @@ class SearchForm(Form):
 from flask import render_template
 from flask import url_for, redirect, request, jsonify
 
+#@app.route("/")
+#def root(complexes=[]):
+#    return render_template('frontpage.html')
+
 @app.route("/")
-def root(complexes=[]):
-    return render_template('frontpage.html')
+def show_complexes(complexes=[]):
+    form = SearchForm()
+    return render_template('show_complexes.html', form=form, complexes=complexes)
 
 @app.route("/h3")
 def show_complexes(complexes=[]):
