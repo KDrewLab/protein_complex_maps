ce8afd9362f244d4ab972004f80d9f80743d39ac
Kevin Drew <ksdrew@uic.edu>
2026-05-19 15:36:01 -0500

    Updated to deal with stemmap node table format

--- protein_complex_maps/complex_map_website/load_db.py
+++ protein_complex_maps/complex_map_website/load_db.py
@@ -25,22 +25,22 @@ def main():
     node_table_file = open(args.node_table,"rb")
     csvreader = csv.reader(node_table_file, delimiter=',', quotechar='"')
     for line in csvreader:
-        #kdrew: header: acc,clustid,clustid_key,genename,key,proteinname,uniprot_link
+        #kdrew: header: acc,clustID_acc,clust_id,Reviewed,Gene Names,Protein names,Organism,Length,uniprot_link
         #kdrew: if header do not parse
         if 'clustID_acc' in line:
             continue
         #print line
         acc = line[0]
-        clustid = line[1]
-        clustid_key = line[2]
-        genename = line[3]
+        clustid_acc = line[1]
+        clustid = line[2]
+        #genename = line[3]
         #geneid = line[4]
-        proteinname = line[4].strip()
-        annotation = line[5]
-        uniprot_link = line[6]
+        proteinname = line[5].strip()
+        #annotation = line[5]
+        uniprot_link = line[8]
 
         #p = cdb.get_or_create(db, cdb.Protein, gene_id = geneid, uniprot_acc=acc, genename=genename, proteinname=proteinname, uniprot_url=uniprot_link)
-        p = cdb.get_or_create(db, cdb.Protein, uniprot_acc=acc, proteinname=proteinname, uniprot_url=uniprot_link, annotation_score=annotation)
+        p = cdb.get_or_create(db, cdb.Protein, uniprot_acc=acc, proteinname=proteinname, uniprot_url=uniprot_link)
         db.session.add(p)
 
         if clustid != '':
